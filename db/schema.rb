99b3568f3f8a650cebbb7128513f923a69c53122
mse_ <mosaelotje@gmail.com>
2016-12-02 02:55:47 +0000

    artidaut

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123183242) do
+ActiveRecord::Schema.define(version: 20161202013118) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               default: "",    null: false
